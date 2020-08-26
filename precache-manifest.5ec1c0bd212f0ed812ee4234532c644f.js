32f0c1e830e52da154a5efe2920df91ed77005a7
Ray <ray644302280@gmail.com>
2020-08-26 16:10:34 +1000

    Updates

---
+++ precache-manifest.5ec1c0bd212f0ed812ee4234532c644f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "321e925f7e7fd7a19ff3ac06a8ab2485",
+    "url": "/Quote-Generator/index.html"
+  },
+  {
+    "revision": "f3eaaf3c4840aecb0170",
+    "url": "/Quote-Generator/static/css/main.e45231a3.chunk.css"
+  },
+  {
+    "revision": "707a1ea19cc9093006e5",
+    "url": "/Quote-Generator/static/js/2.8551e508.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Quote-Generator/static/js/2.8551e508.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3eaaf3c4840aecb0170",
+    "url": "/Quote-Generator/static/js/main.0e71e25c.chunk.js"
+  },
+  {
+    "revision": "e4452e56b3abfc7eb549",
+    "url": "/Quote-Generator/static/js/runtime-main.54df9bc5.js"
+  }
+]);
